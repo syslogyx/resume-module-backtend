a87f44f5839018b05f7b7816f8bfcf1829c763d6
sonal <sonal.kesare@syslogic.com>
2018-12-19 11:53:10 +0530

    some changes in api

--- app/Http/Controllers/BasicScreeningQuestionsController.php
+++ app/Http/Controllers/BasicScreeningQuestionsController.php
@@ -25,7 +25,7 @@ class BasicScreeningQuestionsController extends BaseController
 
         if ($questionsData->first()) {
 
-            return $this->dispatchResponse(200, "", $questionsData);
+            return $this->dispatchResponse(200, "Questions List", $questionsData);
 
         } else {
             return response()->json(['status_code' => 404, 'message' => 'No Records Found!!']);
