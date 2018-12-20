c97193dfdfa4105f5ff7b24be8e9a1455b5b4411
Sonal <sonal@syslogyx.com>
2018-12-20 12:47:57 +0530

    commit after creating background checklist mvc

---
+++ app/BackgroundChecklist.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Validator;
+
+class BackgroundChecklist extends Model
+{
+    //use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'type','mandatory'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    // protected $hidden = [
+    //     'password', 'remember_token',
+    // ];
+
+    protected $table = 'background_check_list';
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    private $rules = array(
+        'name' => 'required',
+        'type' => 'required',
+        'mandatory' => 'required'
+
+    );
+    private $errors;
+
+    public function validate($data) {
+
+        $validator = Validator::make($data, $this->rules);
+        if ($validator->fails()) {
+            $this->errors = $validator->errors();
+            return false;
+        }
+        return true;
+    }
+
+    public function errors() {
+        return $this->errors;
+    }
+}
---
+++ database/migrations/2018_12_20_123824_backgroundChecklist.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BackgroundChecklist extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('background_check_list', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('type');
+            $table->boolean('mandatory');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('background_check_list');
+    }
+}
